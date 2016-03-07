49aeea5951756bb4848158e622e063c956abd832
James McCann <jamesmccann16@gmail.com>
2016-03-07 16:12:50 +0000

    Reading data from json server - products.json & added search capability

--- client/app.js
+++ client/app.js
@@ -3,12 +3,10 @@ var Router = ReactRouter.Router
 var Route = ReactRouter.Route
 var Link = ReactRouter.Link
 var IndexRoute = ReactRouter.IndexRoute
-var ReactDOM = require('react-dom')
-var React = require('react')
-var products = require('./data').allProducts;
+var ReactDOM = require('react-dom') ;
+var React = require('react') ;
+var request = require('superagent') ; 
 
- // var ProductPage = require('./javascript/productPage.js');
- // var Home = require('./javascript/home.js');
 
 var Header = React.createClass({
   render : function() {
@@ -26,112 +24,42 @@ var Header = React.createClass({
 });
 
 
-var NavBar = React.createClass({
-  render : function(){
-     return(
-              <nav className="navbar navbar-default">
-                <div className="container-fluid">
-                  <div className="navbar-header">
-                    <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                      <span className="sr-only">Toggle navigation</span>
-                      <span className="icon-bar"></span>
-                      <span className="icon-bar"></span>
-                      <span className="icon-bar"></span>
-                    </button>
-                  </div>
-                  <div id="navbar" className="navbar-collapse collapse">
-                    <ul className="nav navbar-nav">
-                      <li className="active"><Link to="/home">Home</Link></li>
-                       <li><Link to="/whatsnew">Whats New</Link></li>
-                       <li><Link to="/specials">Specials</Link></li>
-                       <li><Link to="/myaccount">My Account</Link></li>
-                    </ul>
-                  </div>
+var ProductListItem = React.createClass({
+    render : function() {
+         return (
+
+           <div className="col-md-4">
+            <div className="thumbnail">
+              <img src={this.props.product.image}/>
+              <div className="caption">
+                <h4 className="pull-right">
+                  €{this.props.product.price}
+                </h4>
+                <h4><Link to="/product">{this.props.product.name}</Link></h4>
+                                     <p>{this.props.product.description}</p>
                 </div>
-              </nav>
-            );
-  }
-});
+                <div className="ratings">
+                  <p className="pull-right">{this.props.product.reviews} Reviews</p>
+                  <p>
+                    <span className="glyphicon glyphicon-star"></span>
+                    <span className="glyphicon glyphicon-star"></span>
+                    <span className="glyphicon glyphicon-star"></span>
+                    <span className="glyphicon glyphicon-star"></span>
+                    <span className="glyphicon glyphicon-star"></span>
 
-
-var Home = React.createClass({
-  render : function(){
-     return(
-
-      <div>
-
-      <div className="jumbotron">
-  
-          <ul className="tablist" role="tablist">
-            <li role="presentation" className="active"><a href="#featured" aria-controls="featured" role="tab" data-toggle="tab">Featured Item</a></li>
-            <li role="presentation"><a href="#best" aria-controls="best" role="tab" data-toggle="tab">Best Seller</a></li>
-            <li role="presentation"><a href="#discount" aria-controls="discount" role="tab" data-toggle="tab">Summer Discount</a></li>
-            <li role="presentation"><a href="#premium" aria-controls="premium" role="tab" data-toggle="tab">Premium Product</a></li>
-            <li role="presentation"><a href="#gift" aria-controls="gift" role="tab" data-toggle="tab">Gift Idea</a></li>
-          </ul>
-
-         
-          <div className="tab-content">
-            <div role="tabpanel" className="tab-pane active" id="featured">
-              <div className="tab-text">
-                <h2>Apple iPhone 6</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis ornare dolor. Vivamus malesuada auctor est id aliquet. Morbi rutrum lectus sit amet purus pulvinar vehicula</p>
-                <a href="product.html" classNameclassName="btn btn-default">Read More</a>
+                  </p>
+                </div>
               </div>
-              <img className="slide-img" src="img/iphone.png"/>
             </div>
-            <div role="tabpanel" className="tab-pane" id="best">
-              <div className="tab-text">
-                <h2>Xbox One</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis ornare dolor. Vivamus malesuada auctor est id aliquet. Morbi rutrum lectus sit amet purus pulvinar vehicula</p>
-                <a href="product.html" className="btn btn-default">Read More</a>
-              </div>
-              <img className="slide-img" src="img/xbox.png"/>
-            </div>
-            <div role="tabpanel" className="tab-pane" id="discount">
-              <div className="tab-text">
-                <h2>Playstation 4</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis ornare dolor. Vivamus malesuada auctor est id aliquet. Morbi rutrum lectus sit amet purus pulvinar vehicula</p>
-                <a href="product.html" className="btn btn-default">Read More</a>
-              </div>
-              <img className="slide-img" src="img/ps4.png"/>
-            </div>
-            <div role="tabpanel" className="tab-pane" id="premium">
-              <div className="tab-text">
-                <h2>Microsoft Surface</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis ornare dolor. Vivamus malesuada auctor est id aliquet. Morbi rutrum lectus sit amet purus pulvinar vehicula</p>
-                <a href="product.html" className="btn btn-default">Read More</a>
-              </div>
-              <img className="slide-img" src="img/surface.png"/>
-            </div>
-             <div role="tabpanel" className="tab-pane" id="gift">
-              <div className="tab-text">
-                <h2>Vizio Smart TV</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis ornare dolor. Vivamus malesuada auctor est id aliquet. Morbi rutrum lectus sit amet purus pulvinar vehicula</p>
-                <a href="product.html" className="btn btn-default">Read More</a>
-              </div>
-              <img className="slide-img" src="img/tv.png"/>
-             </div>
-          </div>
-        </div>
-    
-         <div>
-         <ProductDisplay products = {this.props.products}/>
-         </div>
-      </div>
-
-      
-    );
-                        
-          
+           )
     }
 });
 
 
-var ProductDisplay = React.createClass({
-    render : function(){
-          var displayedProducts = this.props.products.map(function(product){
-            return <ProductItem key={product.id} products={product}/>;
+var ProductList = React.createClass({
+   render : function(){
+          var displayedProducts = this.props.list.map(function(productItem){
+            return <ProductListItem key={productItem.name} product={productItem}/>;
           });
           return (
 
@@ -146,40 +74,79 @@ var ProductDisplay = React.createClass({
 });
 
 
+var FiltetedProducts = React.createClass({
+    getInitialState : function() {
+        return {
+            searchText : ''
+      }
+    },
 
 
-var ProductItem = React.createClass({
-    render : function () {
-       return (
+    componentDidMount : function() {
+         var that = this ;
+         request.get('http://localhost:3000/products').end(function(error, res){
+              if (res) {
+                var json = JSON.parse(res.text);
+                localStorage.clear();
+                localStorage.setItem('products', JSON.stringify(json)) ;
+                this.setState( {}) ;
+              } else {
+                console.log(error );
+              }
+            }.bind(this)); 
+    },
 
-  
-          <div className="col-md-4">
-            <div className="thumbnail">
-              <img src={this.props.products.image}/>
-              <div className="caption">
-                <h4 className="pull-right">
-                  €{this.props.products.price}
-                </h4>
-                <h4><Link to="/product">{this.props.products.name}</Link></h4>
-                                     <p>{this.props.products.description}</p>
-                </div>
-                <div className="ratings">
-                  <p className="pull-right">{this.props.products.reviews} Reviews</p>
-                  <p>
-                    <span className="glyphicon glyphicon-star"></span>
-                    <span className="glyphicon glyphicon-star"></span>
-                    <span className="glyphicon glyphicon-star"></span>
-                    <span className="glyphicon glyphicon-star"></span>
-                    <span className="glyphicon glyphicon-star"></span>
 
-                  </p>
-                </div>
-              </div>
-            </div>
-        
-      );
+    filterProducts : function(event) {
+        event.preventDefault();
+        this.setState({searchText : event.target.value.toLowerCase()});
+    },
+
+
+    render: function(){
+        var updatedList = localStorage.getItem('products') ? JSON.parse(localStorage.getItem('products')) : [] ;
+
+        updatedList = updatedList.filter(function(productItem){
+
+               return productItem.name.toLowerCase().search(this.state.searchText) !== -1 ;  
+
+         }.bind(this) );
+
+        return (
+          <div>
+           <nav className="navbar navbar-default">
+                <div className="container-fluid">
+                  <div className="navbar-header">
+                    <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                      <span className="sr-only">Toggle navigation</span>
+                      <span className="icon-bar"></span>
+                      <span className="icon-bar"></span>
+                      <span className="icon-bar"></span>
+                    </button>
+                  </div>
+                  <div id="navbar" className="navbar-collapse collapse">
+                    <ul className="nav navbar-nav">
+                      <li className="active"><Link to="/home">Home</Link></li>
+                       
+                       <li><Link to="/specials">Specials</Link></li>
+                       <li><Link to="/myaccount">My Account</Link></li>
+                    </ul>
+                        <ul className="nav navbar-nav navbar-right">
+                         <form className="navbar-form">
+                          <div className="form-group">
+                            <input type="text"  className="form-control" placeholder="Search Products" onChange={this.filterProducts} />
+                          </div>
+                         </form>
+                        </ul>
+                    </div>
+                  </div>
+              </nav>
+               <ProductList list={updatedList} />
+          </div>
+        );
     }
 });
+
 
 var Footer = React.createClass({
   render : function() {
@@ -194,16 +161,13 @@ var Footer = React.createClass({
 });
 
 
-
 var TechApp = React.createClass({
   render : function() {
     return (
       <div>
         <div className="container">
           <Header />
-           <NavBar />
-           <Home products = {this.props.products}/>
-              {this.props.children}
+             <FiltetedProducts/>
          </div>
           <Footer />
       </div>
@@ -213,5 +177,6 @@ var TechApp = React.createClass({
 
 
 ReactDOM.render(
-<TechApp products={products} />,
-document.getElementById('mount-point'));
+  <TechApp/>,
+    document.getElementById('mount-point')
+); 
